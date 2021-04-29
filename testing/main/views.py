dc19c810af245cfaad982f8c560b87957936d5a7
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-29 11:16:32 -0400

    created forms

--- testing/main/views.py
+++ testing/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .forms import PostModelForm
 # Create your views here.
 
 def index(req):
     return render(req, 'index.html')
+
+def newpost(req):
+    if req.method == "POST":
+        post = req.POST or None
+        form = PostModelForm(post)
+        if form.is_valid():
+            form.save()        
