66e60d3a4f9d69f77903e3564b86e59ee4486e3d
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-29 17:32:11 -0400

    changed order pf arguments

--- testing/main/models.py
+++ testing/main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model): 
-    body = models.TextField()
     title = models.CharField(max_length=200)
+    body = models.TextField()
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
