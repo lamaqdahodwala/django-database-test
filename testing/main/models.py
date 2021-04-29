d56d88b1b767ec6a5914436bd43ab62ffab8ee28
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-29 16:56:16 -0400

    created post model

--- testing/main/models.py
+++ testing/main/models.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import User
 
 class Post(models.Model): 
     body = models.TextField()
+    title = models.CharField(max_length=200)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
