d0595fe8ead08f171494ac7545857b50492dd08a
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-30 09:29:38 -0400

    restart

--- testing/main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('newpost', views.newpost, name='newpost'),
-    path('allposts', views.viewposts, name='viewposts')
-]
