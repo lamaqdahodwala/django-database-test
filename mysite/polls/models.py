fbfd9a2d571e6278f9eaa8e4a5e0fba9ea4a6e39
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-03 09:24:47 -0400

    f

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Question(models.Model):
     q = models.TextField()
 
+    def __str__(self):
+        return self.q
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     c_text = models.TextField()
